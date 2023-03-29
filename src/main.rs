e34d638c3ebc789abc86a0484fb8956d43861e83
iablokov-phenobiome <stan@phenobiome.com>
2023-03-29 21:38:52 +0300

    change all return types to Box<dyn Seq>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 mod sequence;
-use crate::sequence::{Sequence, DNA};
+use crate::sequence::{Seq, Sequence, DNA, RNA, Alphabet};
 
 
 fn main() 
 {
-    let dna = Sequence::<DNA>::new("ATGC".to_owned()).unwrap();
-    println!("DNA: {}", dna.sequence);
-    let rna = dna.to_rna();
-    println!("RNA: {}", rna.sequence);
-    let dna = rna.to_dna();
-    println!("DNA: {}", dna.sequence);
+    let dna1 = Sequence::<DNA>::new("ATGC".to_owned()).unwrap();
+    let dna2 = Sequence::<DNA>::new("CGAT".to_owned()).unwrap();
+    let rna1 = dna1.to_rna();
+
+    let sequences: Vec<Box<dyn Seq>> = vec![dna1, rna1, dna2];
+
+
+    for seq in sequences 
+    {
+        println!("{} = {}", seq.seq_type(), seq.sequence());
+        let seq_rev = seq.reverse();
+        println!("{} = {}", seq_rev.seq_type(), seq_rev.sequence());
+    }
 }
--- src/sequence.rs
+++ src/sequence.rs
@@ -1,7 +1,8 @@
 use std::ops::Range;
 
-// This macro creates a boolean map for a given alphabet
 const ALPHABET_SIZE: usize = 128;
+
+// This macro creates a boolean map for a given alphabet
 macro_rules! alphabet_map 
 {
     ($($symbol:expr),*) => {
@@ -17,6 +18,7 @@ macro_rules! alphabet_map
 
 pub trait Alphabet
 { 
+    fn seq_type(&self) -> &str;
     const SYMBOLS: [bool; ALPHABET_SIZE];
     const ALLOWED: [bool; ALPHABET_SIZE];
 
@@ -28,54 +30,75 @@ pub trait Alphabet
 
 pub struct Sequence<A: Alphabet>
 {
-    pub sequence : String,
-    seq_type     : A,
+    sequence : String,
+    pub alphabet : A,
 }
 
 #[derive(Default)]
 pub struct DNA {}
-impl DNA { const name: &str = "DNA";}
-
 impl Alphabet for DNA
 {
+    fn seq_type(&self) -> &str { "DNA" }
     const SYMBOLS: [bool; ALPHABET_SIZE] = alphabet_map!['A', 'T', 'G', 'C'];
     const ALLOWED: [bool; ALPHABET_SIZE] = alphabet_map!['N'];
 }
 
 #[derive(Default)]
 pub struct RNA {}
-impl RNA { const name: &str = "RNA";}
-
 impl Alphabet for RNA
 {
+    fn seq_type(&self) -> &str { "RNA" }
     const SYMBOLS: [bool; ALPHABET_SIZE] = alphabet_map!['A', 'U', 'G', 'C'];
     const ALLOWED: [bool; ALPHABET_SIZE] = alphabet_map!['N'];
 }
 
+
+pub trait Seq
+{
+    fn sequence(&self)     -> String;
+    fn sequence_ref(&self) -> &str;
+    fn seq_type(&self)     -> &str;
+    fn reverse(&self)      -> Box<dyn Seq>;
+}
+
+impl<A: Alphabet + Default + 'static> Seq for Sequence<A>
+{
+    fn sequence(&self)     -> String { self.sequence.clone() }
+    fn sequence_ref(&self) -> &str   { &self.sequence }
+    fn seq_type(&self)     -> &str   { self.alphabet.seq_type() }
+    fn reverse(&self)      -> Box<dyn Seq> 
+    {
+        Box::new( Sequence { sequence : self.sequence.chars().rev().collect(), alphabet : A::default() } )
+    }
+}
+
+
 impl<A: Alphabet + Default> Sequence<A> 
 {
-    pub fn new(sequence: String) -> Option<Self>
+    pub fn new(sequence: String) -> Option<Box<Self>>
     {
         if A::is_word(&sequence)
         {
-            Some( Sequence { sequence : sequence, seq_type : A::default() } )
+            Some(Box::new( Sequence { sequence : sequence, alphabet : A::default() } ))
         }
         else { None }
     }
 }
 
+/// DNA-specific methods
 impl Sequence<DNA>
 {
-    pub fn to_rna(&self) -> Sequence<RNA>
+    pub fn to_rna(&self) -> Box<Sequence<RNA>>
     {
-        Sequence::<RNA> { sequence: self.sequence.replace("T", "U"), seq_type: RNA::default() }
+        Box::new(Sequence::<RNA> { sequence: self.sequence.replace("T", "U"), alphabet: RNA::default() })
     }
 }
 
+/// RNA-specific methods
 impl Sequence<RNA>
 {
-    pub fn to_dna(&self) -> Sequence<DNA>
+    pub fn to_dna(&self) -> Box<Sequence<DNA>>
     {
-        Sequence::<DNA> { sequence: self.sequence.replace("U", "T"), seq_type: DNA::default() }
+        Box::new(Sequence::<DNA> { sequence: self.sequence.replace("U", "T"), alphabet: DNA::default() })
     }
 }
